91957f25484618b2bd62e4ae3c42918602f1bc8e
AbhishekNavgan95 <navganabhishek90@gmail.com>
2025-05-08 13:08:49 +0530

    added cart page

---
+++ src/components/wishlist/CourseCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { IoIosClose } from "react-icons/io";
+import Stars from '../common/Stars';
+import { MdOutlineCurrencyRupee } from 'react-icons/md'
+import { useCartStore } from '@/stores/useCartStore';
+
+const CourseCard = ({
+    course
+}) => {
+
+    const { removeFromCart } = useCartStore()
+
+    const navigate = useNavigate()
+    console.log("course : ", course)
+
+    return (
+        <div className='border flex items-center gap-x-2 rounded-md overflow-hidden border-dark-600'>
+
+            <img className='!aspect-video h-[140px]' src={course?.thumbnail?.url} alt="" />
+
+            <div className='w-full p-2'>
+                <div className='flex justify-between gap-x-4 w-full'>
+                    <h4 onClick={() => navigate(`/course-details/${course?._id}`)} className='line-clamp-1 text-sm hover:underline cursor-pointer font-medium'>{course?.title}</h4>
+                    <button onClick={() => removeFromCart(course?._id)} className='text-xl'>
+                        <IoIosClose />
+                    </button>
+                </div>
+                <p className='text-xs mt-1 text-dark-700'>{course?.category?.name}</p>
+                <p onClick={() => navigate(`/instructor/${course?.instructor?._id}`)} className='text-xs my-1 text-dark-700 hover:text-dark-950 w-max cursor-pointer hover:underline'>
+                    {course?.instructor?.firstName} {course?.instructor?.lastName}
+                </p>
+                <Stars count={4.6} />
+                <div className='flex gap-x-2 mt-1'>
+                    <span className='flex gap-x-1 items-center'>
+                        <MdOutlineCurrencyRupee /> <p>{course?.actualPrice}</p>
+                    </span>
+                    <span className='flex text-xs line-through text-dark-700 gap-x-1 items-center'>
+                        <MdOutlineCurrencyRupee /> <p>{course?.price}</p>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CourseCard
--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -1,8 +1,63 @@
+import { Button } from '@/components/ui/button'
+import CourseCard from '@/components/wishlist/CourseCard'
+import { useCartStore } from '@/stores/useCartStore'
 import React from 'react'
+import { MdOutlineCurrencyRupee } from 'react-icons/md'
 
 const WishList = () => {
+
+  const { cart } = useCartStore()
+
+  const DiscountedPrice = cart?.reduce((acc, course) => acc + Number(course.actualPrice), 0)
+  const totalPrice = cart?.reduce((acc, course) => acc + Number(course.price), 0)
+
+  console.log("cart : ", cart)
   return (
-    <div>WishList</div>
+    <div className='w-full'>
+      <h4 className='text-xl xl:text-3xl font-medium mb-4 md:mb-8'>Your Cart</h4>
+
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4 w-full relative'>
+        <div className='col-span-2 p-4 bg-white rounded-md '>
+          {
+            cart.length > 0 ? (
+              <div className='flex flex-col gap-y-4'>
+                {
+                  cart.map((course, index) => (
+                    <CourseCard key={index} course={course} />
+                  ))
+                }
+              </div>
+            ) : (
+              <div className='w-full h-full flex items-center justify-center'>
+                <span>No courses in your cart</span>
+              </div>
+            )
+          }
+        </div>
+        <div className='col-span-2 p-4 md:col-span-1 w-full h-max sticky top-4 rounded-md bg-white'>
+          <h4 className='text-xl font-medium '>Subtotal</h4>
+          <div className='my-4 space-y-1'>
+            <span className='flex justify-between'>
+              <p>Items</p>
+              <p>{cart?.length || 0}</p>
+            </span>
+
+            <span className='flex justify-between'>
+              <p>Total Price</p>
+              <p className='flex items-center'><MdOutlineCurrencyRupee /> {totalPrice}</p>
+            </span>
+          </div>
+
+          <div className='flex justify-between border-t py-4'>
+            <p>Discounted Price</p>
+            <p className='flex items-center text-xl font-medium'><MdOutlineCurrencyRupee /> {DiscountedPrice}</p>
+          </div>
+
+          <Button className='w-full'>Enroll in all at once</Button>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
